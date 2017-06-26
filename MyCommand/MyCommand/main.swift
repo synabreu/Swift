a5864d6e83e0e53ccdb0c48d3e6b4e026e067f59
Jinho Seo <jinho.seo.165@outlook.com>
2017-06-26 15:18:27 +0900

    Add files via upload
    This is my first swift program I've made it. So this program adds two integers on the console program.

---
+++ MyCommand/MyCommand/main.swift
@@ -0,0 +1,30 @@
+//
+//  FileName : main.swift
+//  Project Name : MyCommand 
+//  Project Documentation : This program adds two integers on the console program.
+//
+//  Created by Jinho Seo on 5/26/16.
+//  Copyright © 2016 Jinho Seo. All rights reserved.
+//
+
+import Foundation
+
+
+var strIntroduction = "This program adds two integers."
+print(strIntroduction)
+
+print("Enter FirstNumber: ")
+let prompt1 =  readLine()
+
+print("Enter SecondNumber: ")
+let prompt2 =  readLine()
+
+if let response1 = prompt1,
+    let response2 = prompt2,
+    let firstNumber = Int(response1),
+    let secondNumber = Int(response2)
+{
+    
+    print("The sum of \(firstNumber) and \(secondNumber) is \(firstNumber + secondNumber)")
+}
+
